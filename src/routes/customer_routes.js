8867e9aa5d4141fcfee0039ecc671d26c0fd32a8
Ted <tedmaruyama@gmail.com>
2022-02-10 15:16:01 -0800

    2nd commit

---
+++ src/routes/customer_routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+const root = '/customers'
+router.get(root,
+    (req, res) => {
+        // do stuff with the request, req
+        console.log(req)
+    
+        // response
+        res.json({
+            name:'Ted'
+        })
+    }
+)
+
+module.exports = {root, router}
