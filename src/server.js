185a3f29d313385237bb07cd2d112c0f8e635069
Ted <tedmaruyama@gmail.com>
2022-02-10 17:23:01 -0800

    Added some endpoints.  Added json parser.

--- src/server.js
+++ src/server.js
@@ -1,28 +1,21 @@
 const path = require('path')
 const express = require('express')
-const {root,router} = require('./routes/customer_routes')
+const {root,router} = require('./routes/customerRouter')
 const app = express()
 const port = 3000
 const apiRoot = '/api'
-console.log(root)
+
+app.use(express.json())
 app.use(express.static(path.join(__dirname,'../www') ))
 app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
+
+
 app.use(apiRoot+root, router)
+
+
 const listenCallback = () => {
     console.log(`Express is listening at http://localhost:${port}`);
 }
 app.listen(port, listenCallback)
-
-app.get('/test',
-    (req, res) => {
-        // do stuff with the request, req
-    
-    
-    // response
-    res.json({
-        name:'Ted'
-    })
-    }
-)
